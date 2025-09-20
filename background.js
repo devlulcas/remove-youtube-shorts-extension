f7f3588527af7ecc4c8c2bfa96f229d6a3f11c19
devlulcas <70298390+devlulcas@users.noreply.github.com>
2025-09-20 19:19:13 -0300

    fix: remove sidebar shortcut

--- background.js
+++ background.js
@@ -1,51 +1,60 @@
+const STATE = {
+  HIDDEN: "ON",
+  VISIBLE: "OFF",
+};
+
+const DEFAULT_STATE = STATE.HIDDEN;
+
+const STATE_KEY = "state";
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ state: 'HIDDEN' });
+  chrome.storage.sync.set({ [STATE_KEY]: DEFAULT_STATE });
 
   chrome.action.setBadgeText({
-    text: 'HIDDEN',
+    text: DEFAULT_STATE,
   });
 });
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    await chrome.storage.sync.get('state', (result) => {
-      if (result.state === 'HIDDEN') {
-        toggleInjection(tabId, 'HIDDEN');
-      } else {
-        console.log('VISIBLE');
-        toggleInjection(tabId, 'VISIBLE');
-      }
-    });
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  if (changeInfo.status !== "complete") {
+    return;
   }
+
+  await chrome.storage.sync.get(STATE_KEY, (result) => {
+    if (result.state === STATE.HIDDEN) {
+      toggleInjection(tabId, STATE.HIDDEN);
+    } else {
+      toggleInjection(tabId, STATE.VISIBLE);
+    }
+  });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
 
-  const nextState = prevState === 'HIDDEN' ? 'VISIBLE' : 'HIDDEN';
+  const nextState = prevState === STATE.HIDDEN ? STATE.VISIBLE : STATE.HIDDEN;
 
   await chrome.action.setBadgeText({
     tabId: tab.id,
     text: nextState,
   });
 
-  chrome.storage.sync.set({ state: nextState });
+  chrome.storage.sync.set({ [STATE_KEY]: nextState });
 
   toggleInjection(tab.id, nextState);
 });
 
 async function toggleInjection(tabId, state) {
-  const css = 'injectable.css';
+  const css = "injectable.css";
 
-  if (state === 'HIDDEN') {
-    await chrome.scripting.insertCSS({
-      files: [css],
-      target: { tabId },
-    });
+  const cssData = {
+    files: [css],
+    target: { tabId },
+  };
+
+  if (state === STATE.HIDDEN) {
+    await chrome.scripting.insertCSS(cssData);
   } else {
-    await chrome.scripting.removeCSS({
-      files: [css],
-      target: { tabId },
-    });
+    await chrome.scripting.removeCSS(cssData);
   }
 }
